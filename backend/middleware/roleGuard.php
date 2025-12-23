28b960084d34ef861ed09fd2478c12748218b5ad
nourtarek885 <nourtarek885@gmail.com>
2025-12-23 18:01:53 +0200

    Update roleGuard.php

--- backend/middleware/roleGuard.php
+++ backend/middleware/roleGuard.php
@@ -10,3 +10,28 @@ Examples:
 Only company → add-flight
 Only passenger → book-flight
 -->
+<?php
+function roleGuard($requiredRole) {
+
+    if(session_status() === PHP_SESSION_NONE)
+    {
+        session_start();
+    }
+    
+    // Check if user role is set in session
+    if (!isset($_SESSION['user_role'])) {
+        http_response_code(403);
+        echo json_encode(["error" => "Access denied. No role assigned."]);
+        exit();
+    }
+
+    // compare roles
+    $currentUserRole = $_SESSION['user_role'];
+
+    if ($currentUserRole !== $requiredRole) {
+        http_response_code(403);
+        echo json_encode(["error" => "Access denied. Insufficient permissions."]);
+        exit();
+    }
+}
+?>
